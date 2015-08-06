debef8c1779140c838f5ebed080de500f6007951
unknown <RH85690@LSC64DITA02C1M5.jetblue.com>
2015-08-06 13:53:42 -0400

    new files and icons

--- js/smallBanner.js
+++ js/smallBanner.js
@@ -1,16 +1,20 @@
-/*Control of elements in the small banner
-on right side of Flight Select*/
 $(document).ready(function () {
-/*	$('#flyFiIcon, #snacksIcon, #awardIcon, #smileyIcon, #legRoomIcon, #awardIcon').mouseover( function(){
-    	$('#dropDown').slideDown();
-    	console.log('mouseOver:::::');
-	})
-	$('#flyFiIcon').mouseleave( function(){
-	    $('#dropDown').slideUp();
-	});
-*/
-	/*$('#flyFiIcon, #snacksIcon, #awardIcon, #smileyIcon, #legRoomIcon, #awardIcon').hover(function(a,b){
-    	$('#dropDown').slideDown();
-    	console.log('hover:::::');
-	})*/
+    mouseEnterHandler = function (event) {
+        var findOverlay = "#overlay" + this.id;
+        $(this).find(findOverlay).fadeIn(500).show();
+        $(this).find('.overlayLegal').fadeIn(500).show();
+        $('.icons').not(this).fadeOut(500).hide().stop(true, true);
+        
+    };
+    mouseLeaveHandler = function (event) {
+      var findOverlay = "#overlay" + this.id;
+        $(this).find(findOverlay).fadeOut(500).hide().stop(true, true);
+        $(this).find('.overlayLegal').fadeOut(500).hide().stop(true, true);
+        $('.icons').not(this).fadeIn(500).show();
+    };
+
+    $('body')
+        .delegate('.icons', 'mouseenter', mouseEnterHandler)
+        .delegate('.icons', 'mouseleave', mouseLeaveHandler);
+
 });
